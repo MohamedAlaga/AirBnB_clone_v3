9fe6dc0156e84e39cbbd3fae8901268dc2235019
MohamedAlaga <malaga.142014@gmail.com>
2024-08-03 16:34:28 +0300

    rewrite app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-from flask import Flask
+"""Flask server (variable app)
+"""
+
+from flask import Flask, jsonify
 from models import storage
 from os import getenv
 from api.v1.views import app_views
-"""
-this is the starting point of v1
-"""
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
+
 
 @app.teardown_appcontext
 def downtear(self):
@@ -18,10 +19,10 @@ def downtear(self):
 
 
 if __name__ == "__main__":
-  host = getenv('HBNB_API_HOST')
-  port = getenv('HBNB_API_PORT')
-  if not host:
-      host = '0.0.0.0'
-  if not port:
-      port = '5000'
-  app.run(host=host, port=port, threaded=True)
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
