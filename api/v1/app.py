75a14dfc3b725dffb1b3fb5b7a96cf58df1da049
MohamedAlaga <malaga.142014@gmail.com>
2024-08-03 16:18:53 +0300

    getting host name and port from the env

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from flask import Flask
 from models import storage
+from os import getenv
 from api.v1.views import app_views
 """
 this is the starting point of v1
@@ -17,5 +18,11 @@ def handleTearDown(exception):
   storage.close()
 
 
-if __name__ == '__main__':
-  app.run(host="0.0.0.0",port=5000,threaded=True)
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST')
+    port = getenv('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
+    app.run(host=host, port=port, threaded=True)
