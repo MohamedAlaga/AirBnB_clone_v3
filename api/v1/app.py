9781342ad1b69fa8cf533bacf3eb0f6b2613c749
MohamedAlaga <malaga.142014@gmail.com>
2024-08-03 16:21:24 +0300

    add a read me fiel for API

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,10 +19,10 @@ def handleTearDown(exception):
 
 
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST')
-    port = getenv('HBNB_API_PORT')
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
+  host = getenv('HBNB_API_HOST')
+  port = getenv('HBNB_API_PORT')
+  if not host:
+      host = '0.0.0.0'
+  if not port:
+      port = '5000'
+  app.run(host=host, port=port, threaded=True)
