d3649e06ae56765e849a3dba6486645a8f67ea99
MohamedAlaga <malaga.142014@gmail.com>
2024-08-03 15:47:11 +0300

    add comments explaining modules

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python3
 from flask import Blueprint
 from api.v1.views.index import *
-
+"""
+this module to define the blue print
+"""
 app_views = Blueprint('/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,6 +1,12 @@
+#!/usr/bin/python3
 from api.v1.views import app_views
 from flask import Flask,jsonify
-
+"""
+this module for defining the routes of the views
+"""
 @app_views.route('/status')
 def index():
+  """
+  route to check the status
+  """
   return jsonify({"status":"OK"})
