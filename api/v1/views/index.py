540aae0540c59857d13ac542be0a492a3cc51d30
MohamedAlaga <malaga.142014@gmail.com>
2024-08-03 16:55:06 +0300

    rewrite status

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,8 +5,7 @@ from flask import jsonify
 this module for defining the routes of the views
 """
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
-def index():
-  """
-  route to check the status
-  """
-  return jsonify({"status":"OK"})
+def status():
+    """ Status of API """
+    return jsonify({"status": "OK"})
+
