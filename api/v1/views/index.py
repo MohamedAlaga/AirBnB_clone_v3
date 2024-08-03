3a9891761c83da9fa12148f49455cf47635ff8bc
MohamedAlaga <malaga.142014@gmail.com>
2024-08-03 17:45:45 +0300

    remove status code

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,4 +6,4 @@ from flask import jsonify,Flask
 @app_views.route('/status', methods=['GET'])
 def status():
     """returns status"""
-    return jsonify({"status": "OK"}), 200
+    return jsonify({"status": "OK"})
